62831b1505a0c022899a76036341364b701cb98e
Gennaro Riccio <gennaro.riccio@gmail.com>
2024-03-03 11:16:58 +0100

    openai

--- src/main.rs
+++ src/main.rs
@@ -1,105 +1,28 @@
-//use fancy::printcol;
-use reqwest::Client;
-use std::error::Error;
-use serde_derive::Deserialize;
-use serde_derive::Serialize;
-// use dialoguer::{theme::ColorfulTheme, Input};
-use serde_json::json;
-use spinners::{Spinner, Spinners};
-use clearscreen;
-use fancy::printcoln;
+use openai_api_rust::*;
+use openai_api_rust::chat::*;
+use openai_api_rust::completions::*;
 
-#[derive(Default, Debug, Clone, PartialEq, Serialize, Deserialize)]
-#[serde(rename_all = "camelCase")]
-pub struct Risposta {
-    pub id: String,
-    pub object: String,
-    pub created: i64,
-    pub model: String,
-    pub choices: Vec<Choice>,
-    pub usage: Usage,
-}
-
-#[derive(Default, Debug, Clone, PartialEq, Serialize, Deserialize)]
-#[serde(rename_all = "camelCase")]
-pub struct Choice {
-    pub index: i64,
-    pub message: Message,
-    #[serde(rename = "finish_reason")]
-    pub finish_reason: String,
-}
-
-#[derive(Default, Debug, Clone, PartialEq, Serialize, Deserialize)]
-#[serde(rename_all = "camelCase")]
-pub struct Message {
-    pub role: String,
-    pub content: String,
-}
-
-#[derive(Default, Debug, Clone, PartialEq, Serialize, Deserialize)]
-#[serde(rename_all = "camelCase")]
-pub struct Usage {
-    #[serde(rename = "prompt_tokens")]
-    pub prompt_tokens: i64,
-    #[serde(rename = "completion_tokens")]
-    pub completion_tokens: i64,
-    #[serde(rename = "total_tokens")]
-    pub total_tokens: i64,
-}
-
-#[derive(Default, Debug, Clone, PartialEq, Serialize, Deserialize)]
-#[serde(rename_all = "camelCase")]
-pub struct LMaConfig {
-    pub messages: Vec<Message>,
-    pub temperature: f64,
-    #[serde(rename = "max_tokens")]
-    pub max_tokens: i64,
-    pub stream: bool,
-}
-#[tokio::main]
-async fn main() -> Result<(), Box<dyn Error>> {
-    clearscreen::clear().expect("failed to clear screen");
-    printcoln!("[green]LLMA-Demo v1.0");
-    printcoln!("[red]Prompt : Sei un esperto di lingua italiana.");
-    
-    // let quest: String = Input::with_theme(&ColorfulTheme::default())
-    //              .with_prompt("Domanda:")
-    //              .interact_text()
-    //              .unwrap();
-
- 
-    let body = json!({
-        "messages": [ 
-           { "role": "system", "content": "sei un esperto di lingua italiana" },
-           { "role": "user", "content": "componi un tema come argomento l'unità d'italia" }
-         ], 
-         "temperature": 0.7, 
-         "max_tokens": -1,
-         "stream": false
-       });
-       
-       
-    
-    println!("{}", body);
-
-    let mut sp = Spinner::new(Spinners::Dots2, "Elaborazione...".into());   
-    let request_url = "http://localhost:1234/v1/chat/completions";                             
-    let response: Risposta = Client::new()
-        .post(request_url)         
-        .json(&body)                
-        .send()
-        .await?
-        .json()
-        .await?;       
-    sp.stop();
-    
-    let con =response.choices[0].message.content.to_string().replace('\n',"").replace("\"", "");
-    println!("");
-    printcoln!("[cyan]{:#?}",con);
-    println!("");
-    println!("");
-
-    
-    
-    Ok(())
+fn main() {
+    // Load API key from environment OPENAI_API_KEY.
+    // You can also hadcode through `Auth::new(<your_api_key>)`, but it is not recommended.
+    let auth = Auth::new("not-needed");
+    let openai = OpenAI::new(auth, "http://localhost:1234/v1");
+    let body = ChatBody {
+        model: "local-model".to_string(),
+        max_tokens: Some(7),
+        temperature: Some(0_f32),
+        top_p: Some(0_f32),
+        n: Some(2),
+        stream: Some(false),
+        stop: None,
+        presence_penalty: None,
+        frequency_penalty: None,
+        logit_bias: None,
+        user: None,        
+        messages: vec![Message { role: Role::User, content: "Hello!".to_string() }],
+    };
+    let rs = openai.chat_completion_create(&body);
+    // let choice = rs.unwrap().choices;
+    // let message = &choice[0].message.as_ref().unwrap();
+    // assert!(message.content.contains("Hello"));
 }
